06b07b25d578e4ce6614208c41bfa35060109746
eilinge <17601329166@163.com>
2019-07-11 15:13:35 +0800

    go WaitGroup

--- _demo/hello5/main.go
+++ _demo/hello5/main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
+	// WaitGroup 对象内部有一个计数器,最初从0开始,它有三个方法：Add(), Done(), Wait() 用来控制计数器的数量。
+	// Add(n) 把计数器设置为n ,Done() 每次把计数器-1 ,wait() 会阻塞代码的运行,直到计数器地值减为0
 	wg := sync.WaitGroup{}
 	chTom := make(chan string)
 	chJerry := make(chan string)
@@ -48,3 +50,18 @@ func main() {
 	chJerry <- "Hello Jerry"
 	wg.Wait()
 }
+
+// func main() {
+//     wg := sync.WaitGroup{}
+//     wg.Add(100)
+//     for i := 0; i < 100; i++ {
+//         go f(i, &wg)
+//     }
+//     wg.Wait()
+// }
+
+// 一定要通过指针传值，不然进程会进入死锁状态
+// func f(i int, wg *sync.WaitGroup) { 
+//     fmt.Println(i)
+//     wg.Done()
+// }
